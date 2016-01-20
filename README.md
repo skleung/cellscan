16d5ff89a4d384829baa92c7d1b5a9cd52d6fa5d
Sherman Leung <shermankleung@gmail.com>
2016-01-20 12:50:23 -0800

    added readme and test images

---
+++ README.md
@@ -0,0 +1,7 @@
+# CellScan Backend
+
+This is the backend service currently hosted at [sherman.pythonanywhere.com](http://sherman.pythonanywhere.com). Here are instructions to get it up and running locally:
+
+1. `pip install -r requirements.txt`
+2. `python server.py`
+3. Go to `localhost:5000` and upload an image. Test images are available in `images/`
--- cv_algorithms.py
+++ cv_algorithms.py
@@ -6,15 +6,13 @@ image_directory = "images"
 
 def count_circles(filepath):
     # read image into numpy array
-    # $ wget http://pythonvision.org/media/files/images/dna.jpeg
     f = open(filepath)
-    dna = scipy.misc.imread(f) # gray-scale image
+    dna = scipy.misc.imread(f) 
 
     # smooth the image (to remove small objects); set the threshold
     dnaf = ndimage.gaussian_filter(dna, 16)
     T = 25 # set threshold by hand to avoid installing `mahotas` or
            # `scipy.stsci.image` dependencies that have threshold() functions
-
     # find connected components
     labeled, nr_objects = ndimage.label(dnaf > T) # `dna[:,:,0]>T` for red-dot case
     return nr_objects
